bbd2f96619504757475622fb7abd8a84ca25060a
ccuervogonz95 <ccuervogonz@outlook.es>
2021-11-30 10:09:21 -0500

    Se crean objetos para los lenguajes de java y python

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,5 +1,14 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo\n");
+        Car car = new Car();
+        car.driver = "Camilo Cuervo";
+        car.license = "GCW294";
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.driver = "Ana Rivera";
+        car2.license = "AFZ394";
+        car2.printDataCar();
     }
 }
--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola mundo")
+    car = Car()
+    car.license = "GCW294"
+    car.driver = "Camilo Cuervo"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "AFX196"
+    car2.driver = "Ana Garcia"
+    print(vars(car2))
